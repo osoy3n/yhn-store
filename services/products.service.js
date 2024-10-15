5517ba83fc85abaf45e1dbe80029259d55c9f263
Yohan D Osorio L <yooslzn1487@gmail.com>
2024-10-14 20:43:37 -0500

    Update mock data

--- services/products.service.js
+++ services/products.service.js
@@ -1,19 +1,17 @@
 const mockProducts = [
   {
-    id: '1',
-    usuario: '159753',
-    contrasena: 'test',
-    estado: '1',
-    id_app_rol: 1,
-    id_empleado: 3
+    id: 'cbce4bbf-edb9-4c91-96f3-5767a4b1e1d0',
+    product_name: 'BenQ-Siemens',
+    email: 'mtomkys0@bing.com',
+    gender: 'Male',
+    ip_address: '149.140.141.215'
   },
   {
-    id: '2',
-    usuario: '456852',
-    contrasena: 'test',
-    estado: '1',
-    id_app_rol: 2,
-    id_empleado: 2
+    id: '8d4d0629-9b52-4b46-aa62-4a51b76dcdcf',
+    product_name: 'alcatel',
+    email: 'kberick1@reverbnation.com',
+    gender: 'Female',
+    ip_address: '122.229.13.6'
   }
 ]
 
