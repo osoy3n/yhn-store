4c38884a26d8071e2c7ddfc3d8cf06ffbcc4731a
Yohan D Osorio L <yooslzn1487@gmail.com>
2024-11-04 19:59:56 -0500

    Separate mock data

---
+++ mocks/products.js
@@ -0,0 +1,18 @@
+const mockProducts = [
+  {
+    id: 'cbce4bbf-edb9-4c91-96f3-5767a4b1e1d0',
+    product_name: 'BenQ-Siemens',
+    email: 'mtomkys0@bing.com',
+    gender: 'Male',
+    ip_address: '149.140.141.215'
+  },
+  {
+    id: '8d4d0629-9b52-4b46-aa62-4a51b76dcdcf',
+    product_name: 'alcatel',
+    email: 'kberick1@reverbnation.com',
+    gender: 'Female',
+    ip_address: '122.229.13.6'
+  }
+]
+
+module.exports = mockProducts
