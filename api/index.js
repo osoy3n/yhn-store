f02e79575f4eb3d00cce8a3209ff0cdeadb57443
Yohan D Osorio L <yooslzn1487@gmail.com>
2024-11-30 16:55:21 -0500

    Refactor user crud

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const routerApi = require('./routes')
+const routerApi = require('./router')
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 
 const app = express()
