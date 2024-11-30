774cbcbbd8a56f89a815ddd771ce05b86bd9c465
Yohan D Osorio L <yooslzn1487@gmail.com>
2024-11-30 10:19:42 -0500

    Add new routes and renaming folder and files

---
+++ api/router/index.js
@@ -0,0 +1,17 @@
+const { Router } = require('express')
+const categoriesRouter = require('./categories.router')
+const ordersRouter = require('./orders.router')
+const productsRouter = require('./products.router')
+const usersRouter = require('./users.router')
+
+function routerApi(app) {
+  const router = Router()
+  app.use('/api/v1', router)
+
+  router.use('/categories', categoriesRouter)
+  router.use('/orders', ordersRouter)
+  router.use('/products', productsRouter)
+  router.use('/users', usersRouter)
+}
+
+module.exports = routerApi
