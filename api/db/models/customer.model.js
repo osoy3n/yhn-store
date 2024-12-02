8a202bfa153b518763bb7a8487dfb722c95c7d07
Yohan D Osorio L <yooslzn1487@gmail.com>
2024-12-01 22:26:39 -0500

    Add modules

---
+++ api/db/models/customer.model.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes, Sequelize } = require('sequelize')
+
+const { USER_TABLE } = require('./user.model')
+
+const CUSTOMER_TABLE = 'customers'
+
+const CustomerSchema = {
+  id: {
+    allowNull: false,
+    primaryKey: true,
+    type: DataTypes.UUID
+  },
+  name: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  lastName: {
+    allowNull: false,
+    field: 'last_name',
+    type: DataTypes.STRING
+  },
+  phone: {
+    allowNull: true,
+    type: DataTypes.STRING,
+  },
+  createdAt: {
+    allowNull: false,
+    defaultValue: Sequelize.NOW,
+    field: 'created_at',
+    type: DataTypes.DATE
+  },
+  userId: {
+    allowNull: false,
+    field: 'user_id',
+    type: DataTypes.UUID,
+    unique: true,
+    references: {
+      key: 'id',
+      model: USER_TABLE
+    },
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+  }
+}
+
+class Customer extends Model {
+  static associate(models) {
+    this.belongsTo(models.User, {as: 'user'})
+  }
+
+  static config(sequelize) {
+    return {
+      modelName: 'Customer',
+      sequelize,
+      tableName: CUSTOMER_TABLE,
+      timestamps: false
+    }
+  }
+}
+
+module.exports = { Customer, CustomerSchema, CUSTOMER_TABLE }
---
+++ api/db/models/product.model.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes, Sequelize } = require('sequelize')
+
+const { CATEGORY_TABLE } = require('./category.model')
+
+const PRODUCT_TABLE = 'products'
+
+const ProductSchema = {
+  id: {
+    allowNull: false,
+    primaryKey: true,
+    type: DataTypes.UUID
+  },
+  name: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+  image: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+  description: {
+    allowNull: false,
+    type: DataTypes.TEXT
+  },
+  price: {
+    allowNull: false,
+    type: DataTypes.INTEGER
+  },
+  createdAt: {
+    allowNull: false,
+    defaultValue: Sequelize.NOW,
+    field: 'created_at',
+    type: DataTypes.DATE
+  },
+  categoryId: {
+    allowNull: false,
+    field: 'category_id',
+    type: DataTypes.UUID,
+    references: {
+      key: 'id',
+      model: CATEGORY_TABLE
+    },
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+  }
+}
+
+
+class Product extends Model {
+  static associate(models) {
+    this.belongsTo(models.Category, { as: 'category' })
+  }
+
+  static config(sequelize) {
+    return {
+      modelName: 'Product',
+      sequelize,
+      tableName: PRODUCT_TABLE,
+      timestamps: false
+    }
+  }
+}
+
+module.exports = { Product, ProductSchema, PRODUCT_TABLE }
