2d920f36e249d1261e4ec64a119d6ffb7562f087
Yohan D Osorio L <yooslzn1487@gmail.com>
2024-12-10 22:51:57 -0500

    Refactor config file and remove dotenv dependency

--- api/config/config.js
+++ api/config/config.js
@@ -1,13 +1,23 @@
-require('dotenv').config()
+process.loadEnvFile()
+
+const {
+  NODE_ENV = 'development',
+  PORT = 3000,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_NAME,
+  DB_PORT
+} = process.env
 
 const config = {
-  env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME,
-  dbPort: process.env.DB_PORT
+  env: NODE_ENV,
+  port: PORT,
+  dbUser: DB_USER,
+  dbPassword: DB_PASSWORD,
+  dbHost: DB_HOST,
+  dbName: DB_NAME,
+  dbPort: DB_PORT
 }
 
 module.exports = { config }
