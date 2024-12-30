228c3570e815032f15d5d58d0f6a4b2a0a5d1a7d
Yohan D Osorio L <yooslzn1487@gmail.com>
2024-12-29 21:06:53 -0500

    Add Order and OrderProduct models with associations to the database

--- api/db/models/index.js
+++ api/db/models/index.js
@@ -1,16 +1,21 @@
 const { Category, CategorySchema } = require('./category.model')
 const { Customer, CustomerSchema } = require('./customer.model')
+const { OrderProduct, OrderProductSchema } = require('./order-product.model')
+const { Order, OrderSchema } = require('./order.model')
 const { Product, ProductSchema } = require('./product.model')
 const { User, UserSchema } = require('./user.model')
 
 function setupModels(sequelize) {
   Category.init(CategorySchema, Category.config(sequelize))
   Customer.init(CustomerSchema, Customer.config(sequelize))
+  OrderProduct.init(OrderProductSchema, OrderProduct.config(sequelize))
+  Order.init(OrderSchema, Order.config(sequelize))
   Product.init(ProductSchema, Product.config(sequelize))
   User.init(UserSchema, User.config(sequelize))
 
   Category.associate(sequelize.models)
   Customer.associate(sequelize.models)
+  Order.associate(sequelize.models)
   Product.associate(sequelize.models)
   User.associate(sequelize.models)
 }
